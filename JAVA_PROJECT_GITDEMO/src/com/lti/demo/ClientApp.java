9d526e3ba7512723be6398ac0d5e683a947396ce
vinayakbangar <vinayakbangar@git.com>
2020-11-19 17:50:18 +0530

    Second Commit- Added New Class

--- JAVA_PROJECT_GITDEMO/src/com/lti/demo/ClientApp.java
+++ JAVA_PROJECT_GITDEMO/src/com/lti/demo/ClientApp.java
@@ -5,7 +5,8 @@ public class ClientApp {
 	public static void main(String[] args) {
 		Person p= new Person("Jay", 22, 555.55);
 		System.out.println(p);
-
+		Person p1= new Person("Vijay", 22, 555.55);
+		System.out.println(p1);
 	}
 
 }
---
+++ JAVA_PROJECT_GITDEMO/src/com/lti/demo/TestApp.java
@@ -0,0 +1,9 @@
+package com.lti.demo;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
